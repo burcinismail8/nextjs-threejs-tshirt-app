137424b95872606534fc876c3d2798d54658c3d6
dev9114537 <dev@activepolitics.bg>
2023-10-28 16:30:26 +0300

    Added T-Shirt 3D Canvas

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,8 +1,26 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-const Canvas = () => {
-  return <div>Canvas</div>;
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      {/* <CameraRig>
+        <Backdrop />
+        
+      </CameraRig> */}
+      <Center>
+        <Shirt />
+      </Center>
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default CanvasModel;
